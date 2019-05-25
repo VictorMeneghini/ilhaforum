a43465248e3ac2fcc311c3161a213e04310b1c42
VictorMeneghini <vhmeneghini@gmail.com>
2019-05-25 09:20:32 -0300

    Create view and templates

--- ilhaforum/settings.py
+++ ilhaforum/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ilhaforum',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': 5432
     }
--- posts/views.py
+++ posts/views.py
@@ -1,2 +1,6 @@
 from django.shortcuts import render
 
+def index(request):
+  forums = Forum.object.all()
+  context['forum'] = forums
+  return render(request, 'movies/index.html')
